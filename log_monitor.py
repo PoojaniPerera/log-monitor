d9adcaf337462c8714091daa0f9c4835af43fd2c
PoojaniPerera <38774890+PoojaniPerera@users.noreply.github.com>
2025-05-22 23:05:58 +0100

    Generate report with warning and errors

--- log_monitor.py
+++ log_monitor.py
@@ -26,16 +26,25 @@ def read_logs_file(input_log_file):
 
 def parse_logs(logs_line_list):
     
+    results = []
     for row in logs_line_list:
         timestamp, description, jobstatus , pid = [item.strip() for item in row]    
                       
         time = datetime.strptime(timestamp, "%H:%M:%S")
-        evaluate_job_duration(time, description, jobstatus , pid)
+        line = evaluate_job_duration(time, description, jobstatus , pid)
+
+        if line is not None:
+            results.append(line)
+    return results
 
 def evaluate_job_duration(time, description, jobstatus , pid):
 
+    line = None
+
     if jobstatus == "START":                       
         job_tracking_dict[pid]['start'] = time
+        
+        
     elif jobstatus == "END":
 
         if "start" in job_tracking_dict[pid]:
@@ -43,23 +52,33 @@ def evaluate_job_duration(time, description, jobstatus , pid):
             duration =  job_tracking_dict[pid]['end'] - job_tracking_dict[pid]['start']
             if  WARNING_TIME_DELTA <= duration <= ERROR_TIME_DELTA:
                 logging.warning(f"PID - {pid} Job {description} took {duration} seconds.Higher than warning Threashold!!")
+                line = f"WARNING : PID - {pid} Job {description} took {duration} seconds.Higher than warning Threashold!!"
             elif duration >  ERROR_TIME_DELTA:
                 logging.error(f"PID - {pid} Job {description} took {duration} seconds.Higher than error Threashold!!")
+                line = f"ERROR : PID - {pid} Job {description} took {duration} seconds.Higher than error Threashold!!"
             else:
                 logging.info(f"PID - {pid} Job {description} took {duration} seconds. Normal duration")
 
         else:
-            logging.info(f"no start job found for job {description} PID - {pid}")
+                logging.info(f"no start job found for job {description} PID - {pid}")
+        
     else:
         logging.error(f"invalid job status!")    
+    return line  
 
 
+def generate_report(results, output_path='report.txt'):
+
+    with open(output_path, 'w') as f:
+        for line in results:
+            f.write(line + '\n')
 
 
 def main():
     input_log_file = "logs.log"
     logs_line_list = read_logs_file(input_log_file)
-    parse_logs(logs_line_list)
+    warning_error_list = parse_logs(logs_line_list)
+    generate_report(warning_error_list)
     
 if __name__ == '__main__':
     main()
