d82abfd1be230543ac3540429bb598dfddf84edb
PoojaniPerera <38774890+PoojaniPerera@users.noreply.github.com>
2025-05-22 23:44:17 +0100

    Add unit tests and documentation

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # log-monitor
 A python based app to monitor job duration from log files and print alerts
+
+## Description
+
+This application parses a log file containing job start and end events, calculates job durations, and generates alerts based on how long each job takes.
+
+### Features
+
+- Parses CSV log files
+- Tracks job duration from START to END
+- Logs a:
+  - **Warning** if a job exceeds 5 minutes
+  - **Error** if a job exceeds 10 minutes
+- Outputs both to `job_monitor.log` and a summary `report_<timestamp>.txt`
+
+## Usage
+
+### Run with sample logs:
+
+python log_monitor.py
+
+#### Unit test command:
+python -m unittest test_log_monitor.py
+
+
+## Improvements
+- command line argument to pass log file name
+- log rotate mechanism for job_monitor log
+- Add more unit tests
+- More user friendly report structure
---
+++ test_log_monitor.py
@@ -0,0 +1,38 @@
+# tests/test_log_parser.py
+
+import unittest
+from log_monitor import parse_logs
+
+class TestLogParserUnit(unittest.TestCase):
+
+    def test_warning_duration(self):
+        logs = [
+            ["08:00:00", "Job A", "START", "1"],
+            ["08:06:00", "Job A", "END", "1"]
+        ]
+        actual_result = parse_logs(logs)
+        expected_result = 'WARNING :'
+        
+        self.assertIn(expected_result, actual_result[0])
+
+    def test_error_duration(self):
+        logs = [
+            ["08:00:00", "Job B", "START", "2"],
+            ["08:15:01", "Job B", "END", "2"]
+        ]
+        
+        actual_result = parse_logs(logs)
+        expected_result = 'ERROR :'
+        
+        self.assertIn(expected_result, actual_result[0])
+
+    def test_no_output_for_short_job(self):
+        logs = [
+            ["08:00:00", "Job C", "START", "3"],
+            ["08:03:00", "Job C", "END", "3"]
+        ]
+        expected_result = parse_logs(logs)
+        self.assertEqual(len(expected_result), 0)
+
+if __name__ == "__main__":
+    unittest.main()
